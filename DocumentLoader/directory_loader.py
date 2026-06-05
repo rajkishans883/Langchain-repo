c851390e9f17319bb347d8491a6e0a8ef0e9dfe5
rajkishans883 <rajkishans883@gmail.com>
2026-06-05 13:28:42 +0530

    dpunloader

---
+++ DocumentLoader/directory_loader.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import DirectoryLoader,PyPDFLoader
+from langchain_mistralai import ChatMistralAI
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel, RunnableSequence
+from pydantic import BaseModel, Field
+from typing import Literal
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+mistral_key = os.getenv("MISTRAL_API_KEY")
+
+# Initialize models
+model = ChatMistralAI(
+    api_key=mistral_key,
+    model_name="mistral-tiny",
+    temperature=0.3, 
+    max_tokens=100       
+)
+
+loader= DirectoryLoader(
+    path="collagedoc",
+    glob="*.pdf",
+    loader_cls=PyPDFLoader
+)
+
+doc=loader.load()
+
+print(len(doc))
+
+print(doc[0].page_content)
+print(doc[0].metadata)
